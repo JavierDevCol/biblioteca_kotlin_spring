4286b9b2b63cef1c3822053dca24166c4c57ca26
ONAD <javiergarciadevcol@gmail.com>
2022-09-12 18:05:08 -0500

    primir commi

---
+++ src/test/kotlin/com/onad/libreria_kotlin/LibreriaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onad.libreria_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibreriaKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
